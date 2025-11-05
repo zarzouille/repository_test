e4a6b11fe520ca7831d7cff686456c17dfd59e89
Xavier <xcollongues@degetel.com>
2025-11-05 16:55:46 +0100

    update app )

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def load_config():
         "height": 200,
         "background_color": "#F5F5F5",
         "text_color": "#222222",
-        "font_path": "/Library/Fonts/Arial.ttf",  # MacOS font par défaut
+        "font_path": "/System/Library/Fonts/Supplemental/Arial.ttf",  # MacOS font par défaut
         "font_size": 40,
         "message_prefix": "Temps restant : ",
         "target_date": "2025-12-31T23:59:59",
