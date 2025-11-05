c28c4ed3bdfbaeeed68a5a5cc2773c22b11508c3
Xavier <xcollongues@degetel.com>
2025-11-05 15:25:42 +0100

    update app port

--- app.py
+++ app.py
@@ -130,5 +130,5 @@ def countdown_gif():
     return send_file(buf, mimetype="image/gif")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
